8f64cac8ce55bc084a1dd3dd995d971667cf846e
m-takahashi <m-takahashi@cichdgroup.com>
2026-02-05 13:42:40 +0900

    商品詳細表示画面

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("admin/products", "routes/admin.products.tsx"),
+    route("admin/products", "routes/admin.products.tsx"),
+    route("about", "routes/about.tsx"),
   route("api/upload-product-image", "routes/api.upload-product-image.tsx"),
 ] satisfies RouteConfig;
